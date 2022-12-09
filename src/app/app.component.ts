a9ad494890c0ed91af97fa4b8e78db220aa5bfa0
Well Luiz <wellwlds@hotmail.com>
2022-12-09 16:26:39 -0300

    atualizando design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,6 +42,28 @@ export class AppComponent {
       }
     },
   }
+  customOportunities: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 2
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 3
+      }
+    },
+  }
   customFeedback: OwlOptions = {
     loop: true,
     mouseDrag: true,
